8f25d1bcc43301ef3cb009e7fdd28421d9d57642
Ilya Eskov <eskovilya@gmail.com>
2016-03-14 12:46:15 +0300

    Исправление метода Эйлера
    Теперь метод Эйлера можно применять на любой функции

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def v( x ):
 	"""
 	Первая производная функции
 	"""
-	du = derivative(u)
-	return du(x)
+	du = derivative( u )
+	return du( x )	
 
-def derivative( func ):
+def derivative( functinon ):
 	"""
 	Функция для вычисления производной
 	"""
 	def df( x, h = 0.1e-5 ):
-		return ( func( x + h / 2 ) - func( x - h / 2) ) / h
+		return ( functinon( x + h / 2 ) - functinon( x - h / 2) ) / h
 	return df
 
 def grid( a, b, n ):
@@ -43,17 +43,24 @@ def setUnVn( xn ):
 	un, vn = [], []
 
 	for it in xn:
-		un.append(round(u(it), 2) )
-		vn.append(round(v(it), 2) )
+		un.append( round( u( it ), 2 ) )
+		vn.append( round( v( it ), 2 ) )
 	print 'yn =', un 
 	print 'zn =', vn
 	return un, vn
 
-def f1(x, u, v):
-	return v
+def f1( x, u, v ):
+	'''
+	du/dx = f1( x, u, v )
+	'''
+	return v 
 
-def f2(x, u, v):
-	return 6 * x	
+def f2( x, u, vv ):
+	'''
+	dv/dx = f2( x, u, v )
+	'''
+	dv = derivative( v ) 
+	return dv( x ) 
 
 def methodEuler( x0, y0, z0, h, n):
 	'''
@@ -62,21 +69,21 @@ def methodEuler( x0, y0, z0, h, n):
 	yn, zn = [], []
 	for each in xrange( 1, n ):
 
-		y1 = y0 + f1(x0, y0, z0) * h
-		z1 = z0 + f2(x0, y0, z0) * h
+		y1 = y0 + f1( x0, y0, z0 ) * h
+		z1 = z0 + f2( x0, y0, z0 ) * h
 
-		yn.append(round(y1, 2))
-		zn.append(round(z1, 2))
+		yn.append( round( y1, 2 ) )
+		zn.append( round( z1, 2 ) )
 
 		x0 = x0 + h
 		y0 = y1
 		z0 = z1
+	print 'yn =', yn 
+	print 'zn =', zn	
 	return yn, zn		
-	
+
 xn = grid( 0, 1, 10 ) 
-print '---------------Exact number---------------'
-unnew, vnnew = setUnVn(xn)
+print '-' * 20, 'Exact number', '-' * 20
+unnew, vnnew = setUnVn( xn )
+print '-' * 20, 'Eulers method', '-' * 20
 un, vn = methodEuler( 0, 0, 2, 0.1, 10 )
-print '---------------Eulers method--------------'
-print 'yn =', un 
-print 'zn =', vn

a0173c34c9c350fe5fa17e80529c6b77a7873774
Ilya Eskov <eskovilya@gmail.com>
2016-04-18 10:43:47 +0300

    Метод Эйлера исправлен
    -который не работал ничерта

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
+import numpy as np
 
-constn = 100
-a, b = 0, 1
-consth = round( a + b , 2) / constn
+a = 0.
+b = 1.
+n = 10
 
 def u( x ):
 	"""
@@ -16,71 +17,52 @@ def v( x ):
 	Первая производная функции
 	"""
 	du = derivative( u )
-	return du( x )	
+	return du( x )
+
+def dv( x ):
+	dv = derivative(v)
+	return dv(x)
 
 def derivative( functinon ):
 	"""
 	Функция для вычисления производной
 	"""
-	def df( x, h = 0.1e-5 ):
+	def df( x, h = 0.1e-3 ):
 		return ( functinon( x + h / 2 ) - functinon( x - h / 2) ) / h
-	return df
+	return df	
 
 def grid( a, b, n ):
 	"""
 	Создание сетки xn, размерность = n 
 	"""
 	arrayOfX = []
-	h = round( a + b , 1) / n
-	lv = a
+	h = ( a + b ) / n
+	print 'h =', h
+	lv = b
+	arrayOfX.append(b)
 
-	for num in range( 1, n ):
-		lv = lv + h
-		arrayOfX.append( round( lv, 1) )
+	for num in range( n, 0, -1 ):
+		lv = lv - h
+		arrayOfX.append( round( lv, 3) )
 
-	return arrayOfX	
+	print 'xn =', arrayOfX
+	return arrayOfX, h	
 
-def setUnVn( xn ):
-	'''
-	Создание un, vn
-	'''
-	un, vn = [], []
-
-	for it in xn:
-		un.append( round( u( it ), 2 ) )
-		vn.append( round( v( it ), 2 ) )
-	un.reverse()
-	vn.reverse()	
-	print 'yn =', un
-	print 'zn =', vn
-	return un, vn
-
-def f1( x, u, v ):
-	'''
-	du/dx = f1( x, u, v )
-	'''
-	return v 
-
-def f2( x, u, vv ):
-	'''
-	dv/dx = f2( x, u, v )
-	'''
-	dv = derivative( v ) 
-	return dv( x ) 
-
-def methodEuler( xlast, ylast, zlast, h, n):
+def methodEuler( xlast, ylast, zlast, h, n ):
 	'''
 	Реализация метода Эйлера
 
 	'''
 	yn, zn = [], []
-	for each in xrange( n, 1, -1):
+	yn.append( ylast )
+	zn.append( zlast )
+	for each in xrange( n - 1, 1, -1):
 
-		ycurrent = ylast - f1( xlast, ylast, zlast ) * h
-		zcurrent = zlast - f2( xlast, ylast, zlast ) * h
+		ycurrent = ylast - v( xlast ) * h
+		zcurrent = zlast - dv( xlast ) * h
 
-		yn.append( round( ycurrent, 2 ) )
-		zn.append( round( zcurrent, 2 ) )
+		yn.append( round( ycurrent, 3 ) )
+		zn.append( round( zcurrent, 3 ) )
 
 		xlast = xlast - h
 		ylast = ycurrent
@@ -88,10 +70,7 @@ def methodEuler( xlast, ylast, zlast, h, n):
 
 	print 'yn =', yn 
 	print 'zn =', zn	
-	return yn, zn		
+	return yn, zn			
 
-xn = grid( 0, 1, constn ) 
-print '-' * 20, 'Exact number', '-' * 20
-unnew, vnnew = setUnVn( xn )
-print '-' * 20, 'Eulers method', '-' * 20
-un, vn = methodEuler( 1, 3, 5, consth, constn )
+xn, h = grid(a, b, n)
+methodEuler(b, u(b), round(v(b), 3), h, n)	
